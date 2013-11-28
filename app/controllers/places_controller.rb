80ecc2170f9d3088dd2387d5c8e343380751cda6
Anton Shabanov <me.antony.rain@gmail.com>
2013-11-28 18:28:36 +0200

    Places Resourse

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,22 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
-    render text: @places.map{ |p| "#{p.id} -> #{p.name} -> #{p.address} -> #{p.price_per_hour.to_i.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1 ")}"}.join('<br/>')
+    # render text: @places.map{ |p| "#{p.id} -> #{p.name} -> #{p.address} -> #{p.price_per_hour.to_i.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1 ")}"}.join('<br/>')
   end
 
+  # /places/:id GET
+  def show
+  end
+
+  # /places/new
+  def new
+  end
+
+  # /places/:id/edit GET
+  def edit
+  end
+
+  # /places POST
   def create
     name = params[:name]
     address = params[:address]
@@ -18,5 +31,13 @@ class PlacesController < ApplicationController
     @place.save()
 
     render text: "<b>TODO: </b>place create #{@place.id}<br/>" + "Make request like this http://localhost:3000/places/create?name=Test+Name&address=Test+Address&price_per_hour=2000&description=Test+Description"
+  end
+
+  # /places/:id PUT
+  def update
+  end
+
+  # /places/:id DELETE
+  def destroy
   end
 end
