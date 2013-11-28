160852abcc389aff189c4790d27f52b3cead1497
Anton Shabanov <me.antony.rain@gmail.com>
2013-11-28 16:31:20 +0200

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samplace::Application.config.secret_token = '5a2231e5c5ffa1ec3c3ae4fc003d93228c346f3fd3f197deec93f1b37d93b4fd2198b1e4f178a05053276357c85646dc8765a7f6902e87777bf0fec993d296f6'
